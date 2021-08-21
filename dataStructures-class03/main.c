bb770b73eff6f59bf3da091f672dd8978d00c580
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-20 23:42:59 -0300

    Initial Commit

---
+++ dataStructures-class03/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  dataStructures-class03
+//
+//  Created by Luiz Carlos da Silva Araujo on 20/08/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
